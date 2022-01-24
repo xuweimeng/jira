e7fb95b83d5b2c6ffa6d0891165f52a639c5110f
weimeng <xu13071061935@163.com>
2022-01-24 15:23:04 +0800

    登录注册样式

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,19 +1,40 @@
 import React, { useState } from "react";
 import RejisterScreen from "unauthenticated-app/register";
 import LoginScreen from "unauthenticated-app/login";
-import { Button, Card } from "antd";
+import { Button, Card, Divider } from "antd";
+import styled from '@emotion/styled'
 
 const UnauthenticatedApp = () => {
   const [isRegister, setIsgister] = useState(false);
 
   return (
-    <Card style={{width: '500px', margin: '10% auto 0'}}>
-      {isRegister ? <RejisterScreen /> : <LoginScreen />}
-      <Button type="primary" onClick={() => setIsgister(!isRegister)}>
-        切换至{isRegister ? "登录" : "注册"}
-      </Button>
-    </Card>
+    <Container>
+      <ShadowCard>
+        {isRegister ? <RejisterScreen /> : <LoginScreen />}
+        <Divider  />
+        <a onClick={() => setIsgister(!isRegister)}>
+          {isRegister ? "已有账号？直接登录" : "注册新账号"}
+        </a>
+      </ShadowCard>
+    </Container>
   );
 };
+
+const ShadowCard = styled(Card)`
+width: 40rem;
+min-height: 56rem;
+padding: 3.2rem 4rem;
+border-radious: 0.3rem;
+box-sizing: border-box;
+box-shadow: rgba(0,0,0,0.1) 0 0 10px;
+text-align: center;
+`
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 100vh
+`
 
 export default UnauthenticatedApp;
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -12,21 +12,19 @@ const LoginScreen = () => {
   return (
     <Form onFinish={onSubmit}>
       <Item
-        label="用户名"
         name="username"
         rules={[{ required: true, message: "用户名不能为空！" }]}
       >
         <Input type="text" placeholder="用户名" />
       </Item>
       <Item
-        label="密码"
         name="password"
         rules={[{ required: true, message: "用户名不能为空！" }]}
       >
         <Input type="password" placeholder="用户名" />
       </Item>
       <Item>
-        <Button type="primary" htmlType="submit">
+        <Button type="primary" htmlType="submit" block>
           登录
         </Button>
       </Item>
